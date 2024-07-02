2aca9650a50e90ceb885fcaada6fb88d32ab6ba1
Amada <470014@clases.edu.sv>
2024-07-02 14:16:29 -0600

    Agregar archivos de proyecto.

---
+++ 06-xamarin4700154/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace _06_xamarin4700154
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ 06-xamarin4700154/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace _06_xamarin4700154
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
